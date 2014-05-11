0bd0c9c97f922715dea94eb08f5c7b70f9c665a0
Michael Donat <michael@monitechnologies.com>
2014-05-12 00:02:49 +0100

    auto-generated API docs for 1.1.x:e3ff0cc958d358d2b4236af8babee3788b063c64

---
+++ api/v0.1.1/tree.js
@@ -0,0 +1,16 @@
+var tree = [["VirtualFileSystem","VirtualFileSystem.html","",
+                [["Structure","VirtualFileSystem\/Structure.html","",
+                [["Directory","VirtualFileSystem\/Structure\/Directory.html"," < Node",
+                [        ]],["File","VirtualFileSystem\/Structure\/File.html"," < Node",
+                [        ]],["Node","VirtualFileSystem\/Structure\/Node.html","",
+                [        ]],["Root","VirtualFileSystem\/Structure\/Root.html"," < Directory",
+                [        ]]        ]],["Wrapper","VirtualFileSystem\/Wrapper.html","",
+                [["FileHandler","VirtualFileSystem\/Wrapper\/FileHandler.html","",
+                [        ]]        ]],["Container","VirtualFileSystem\/Container.html","",
+                [        ]],["Factory","VirtualFileSystem\/Factory.html","",
+                [        ]],["FileExistsException","VirtualFileSystem\/FileExistsException.html"," < Exception",
+                [        ]],["FileSystem","VirtualFileSystem\/FileSystem.html","",
+                [        ]],["NotFoundException","VirtualFileSystem\/NotFoundException.html"," < Exception",
+                [        ]],["Wrapper","VirtualFileSystem\/Wrapper.html","",
+                [        ]]        ]]        ]
+
